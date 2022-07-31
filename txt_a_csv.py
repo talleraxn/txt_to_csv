ccdc2bb7bef1da125a714dd9b86422df095c7121
unknown <talleraxn@gmail.com>
2022-07-30 23:50:13 -0300

    new file:   infoBat2.xlsx 	modified:   txt_a_csv.py

--- txt_a_csv.py
+++ txt_a_csv.py
@@ -1,8 +1,8 @@
-"""
+import csv
 from openpyxl import Workbook, load_workbook
 libro = Workbook()
 hoja=libro.active
-"""
+
 
 contenido_archivo_txt=[]
 archivo_txt=open("infoBat2.txt","r")
@@ -23,7 +23,19 @@ with open("infoBat2.csv","w") as archivo_csv:
         archivo_csv.write(contenido_archivo_txt[linea_numero])
         linea_numero = linea_numero + 1
 
-"""
-libro=load_workbook('infoBat2.csv')
+
+#libro=load_workbook('infoBat2.csv')
+
+with open('infoBat2.csv') as archivo:
+    reader = csv.reader(archivo, delimiter=';')
+    for row in reader:
+        hoja.append(row)
+
+cabecera = hoja.append(["Fecha", "Hora", "V_3.3", "Corriente","V_Bat","x","V_Panel"])
+
+ultima_fila=hoja.max_row
+cabecera_nombre="A"+str(ultima_fila)+":G"+str(ultima_fila)
+print(cabecera_nombre)
+hoja.move_range(cabecera_nombre, rows=-ultima_fila+1, cols=0)
+
 libro.save('infoBat2.xlsx')
-"""
